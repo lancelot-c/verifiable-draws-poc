ea9de4506351ae57007aa3643d592aad97c9f352
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2023-07-13 10:44:09 +0200

    added errors & getters

--- back/contracts/VerifiableDraws.sol
+++ back/contracts/VerifiableDraws.sol
@@ -15,24 +15,21 @@ import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol"; // Ownership
  */
 contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, ConfirmedOwner {
 
+    /*** Errors ***/
+
+    error DrawAlreadyExists(string cid);
     error DrawDoesNotExist(string cid);
     error DrawTooEarly(string cid);
+    error DrawAlreadyTriggered(string cid);
+    error DrawAlreadyCompleted(string cid);
+    error NoRandomWordNeeded(string[] cids);
+    error RequestDoesNotExist(uint256 id);
+    error RequestAlreadyFulfilled(uint256 id);
+    error RequestFulfilledButEmpty(uint256 id);
 
-    struct Draw {
-        uint64 publishedAt; // block number at which the draw was published on the contract
-        uint64 scheduledAt; // timestamp at which the draw should be triggered
-        uint256 occuredAt; // block number at which the draw has occurred
-        uint32 entropyNeeded; // number of bytes of information needed to compute winners
-        bool entropyPending;
-        bytes entropy; // entropy used to pick winners
-        bool completed;
-    }
-   
-    // Draws 
-    mapping(string => Draw) private draws; // Content Identifier (CID) => Draw
-    string[] private pendingDraws;
 
-    // Events
+    /*** Events ***/
+
     event DrawLaunched(string cid, uint64 publishedAt, uint64 scheduledAt, uint32 entropyNeeded);
     event DrawsTriggered(string[] performData);
     event RequestSent(
@@ -47,33 +44,52 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
     event RequestFulfilled(uint256 requestId, uint256[] randomWords);
     event DrawCompleted(string cid, bytes extractedEntropy);
 
+
+    /*** Draws ***/
+
+    struct Draw {
+        uint64 publishedAt; // block number at which the draw was published on the contract
+        uint64 scheduledAt; // timestamp at which the draw should be triggered
+        uint256 occuredAt; // block number at which the draw has occurred
+        bytes entropy; // entropy used to pick winners
+        uint32 entropyNeeded; // number of bytes of information needed to compute winners
+        bool entropyPending; // when the random numbers are being generated
+        bool completed; // when the draw is done and entropy as been filled
+    }
+   
+    mapping(string => Draw) private draws; // Content Identifier (CID) => Draw
+    string[] private pendingDraws; // draws for which completed = false
+    uint32 private drawCount = 0;
+
+
+    /*** Requests ***/
+
     struct RequestStatus {
         bool exists; // whether a requestId exists
         bool fulfilled; // whether the request has been successfully fulfilled
         uint256[] randomWords;
         string[] cids;
     }
+
     mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
 
+
+    /*** VRF ***/
+    
     VRFCoordinatorV2Interface COORDINATOR;
+    uint64 private s_subscriptionId;
 
     // For other networks see https://docs.chain.link/docs/vrf-contracts/#configurations
-    address link_token_contract = 0xb0897686c545045aFc77CF20eC7A532E3120E0F1;
-    address vrfCoordinator = 0xAE975071Be8F8eE67addBC1A82488F1C24858067;
+    address link_token_contract = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
     // Gas lane to use, which specifies the maximum gas price to bump to
-    bytes32 keyHash = 0x6e099d640cde6de9d40ac749b4b594126b0169747122711109c9985d47751f93;
+    bytes32 keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
     
-
-    // Your subscription ID.
-    uint64 private s_subscriptionId;
-    
-
     // Depends on the number of requested values that you want sent to the
     // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
-    // so 100,000 is a safe default for this example contract. Test and adjust
+    // so 100,000 is a safe default for this contract. Test and adjust
     // this limit based on the network that you select, the size of the request,
-    // and the processing of the callback request in the fulfillRandomWords()
-    // function.
+    // and the processing of the callback request in the fulfillRandomWords() function.
     uint32 callbackGasLimit = 1000000;
 
     // The default is 3, but you can set this higher.
@@ -90,6 +106,7 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
         s_subscriptionId = subscriptionId;
     }
 
+
     function launchDraw(
         string memory cid,
         uint64 scheduledAt,
@@ -98,15 +115,20 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
         external
         onlyOwner
     {
-        require(draws[cid].publishedAt == 0, "Draw already exists");
+        if (draws[cid].publishedAt != 0) {
+            revert DrawAlreadyExists(cid);
+        }
+
         uint64 publishedAt = uint64(block.number);
         uint256 occuredAt = 0;
         bytes memory entropy = "";
-        draws[cid] = Draw(publishedAt, scheduledAt, occuredAt, entropyNeeded, false, entropy, false);
+        draws[cid] = Draw(publishedAt, scheduledAt, occuredAt, entropy, entropyNeeded, false, false);
         pendingDraws.push(cid);
+        drawCount++;
         emit DrawLaunched(cid, publishedAt, scheduledAt, entropyNeeded);
     }
     
+
     function checkUpkeep(
         bytes calldata /* checkData */
     )
@@ -120,12 +142,12 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
         uint32 count = 0;
 
         for (uint64 i = 0; i < pendingDraws.length; i++) {
-            string memory cid = pendingDraws[i];
-            if (block.timestamp >= draws[cid].scheduledAt && !draws[cid].entropyPending) {
-                if (!upkeepNeeded) {
-                    upkeepNeeded = true;
-                }
 
+            string memory cid = pendingDraws[i];
+
+            // Conditions needed in order to trigger a draw
+            if (draws[cid].publishedAt > 0 && block.timestamp >= draws[cid].scheduledAt && !draws[cid].entropyPending && !draws[cid].completed) {
+                upkeepNeeded = true;
                 tempDraws[i] = cid;
                 count++;
             }
@@ -155,16 +177,27 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
     {
         string[] memory requestedDraws = abi.decode(performData, (string[]));
 
+        // We revalidate the draws in the performUpkeep to prevent malicious actors
+        // from calling performUpkeep with wrong parameters 
         for (uint64 i = 0; i < requestedDraws.length; i++) {
+
             string memory cid = requestedDraws[i];
             if (draws[cid].publishedAt == 0) {
                 revert DrawDoesNotExist(cid);
             }
 
-            // We revalidate the upkeep in the performUpkeep function to prevent malicious actors
-            // from triggering draws which have not yet reached their scheduled date
             if (block.timestamp < draws[cid].scheduledAt) {
                 revert DrawTooEarly(cid);
+            }
+
+            if (draws[cid].entropyPending) {
+                revert DrawAlreadyTriggered(cid);
+            } else {
+                draws[cid].entropyPending = true;
+            }
+
+            if (draws[cid].completed) {
+                revert DrawAlreadyCompleted(cid);
             }
         }
 
@@ -172,12 +205,6 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
         generateEntropyFor(requestedDraws);
     }
 
-    function setSubscription(uint64 subscriptionId) external onlyOwner {
-        s_subscriptionId = subscriptionId;
-    }
-
-
-    // Assumes the subscription is funded sufficiently.
     function generateEntropyFor(string[] memory triggerDraws)
         private
         returns (uint256 requestId)
@@ -186,7 +213,6 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
         uint32 totalEntropyNeeded = 0;
         for (uint64 i = 0; i < triggerDraws.length; i++) {
             totalEntropyNeeded += draws[triggerDraws[i]].entropyNeeded;
-            draws[triggerDraws[i]].entropyPending = true;
         }
  
         // Each word gives an entropy of 256 bits (see _randomWords in fulfillRandomWords)
@@ -194,6 +220,10 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
         uint32 entropyPerWord = 32;
         // Number of random words to generate
         uint32 numWords = divisionRoundUp(totalEntropyNeeded, entropyPerWord);
+
+        if (numWords == 0) {
+            revert NoRandomWordNeeded(triggerDraws);
+        }
 
         // Will revert if subscription is not set and funded.
         requestId = COORDINATOR.requestRandomWords(
@@ -229,7 +259,19 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
         uint256[] memory _randomWords
     ) internal override {
         RequestStatus memory request = s_requests[_requestId];
-        require(request.exists, "request not found");
+
+        if (!request.exists) {
+            revert RequestDoesNotExist(_requestId);
+        }
+
+        if (request.fulfilled) {
+            revert RequestAlreadyFulfilled(_requestId);
+        }
+
+        if (_randomWords.length == 0) {
+            revert RequestFulfilledButEmpty(_requestId);
+        }
+
         request.fulfilled = true;
         request.randomWords = _randomWords;
         emit RequestFulfilled(_requestId, _randomWords);
@@ -241,32 +283,63 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
         for (uint64 i = 0; i < request.cids.length; i++) {
             string memory cid = request.cids[i];
 
-            if (draws[cid].completed == false) {
-
+            if (!draws[cid].completed) {
                 bytesNeeded = draws[cid].entropyNeeded;
                 bytes memory extractedEntropy = extractBytes(totalEntropy, from, bytesNeeded);
                 draws[cid].entropy = extractedEntropy;
                 draws[cid].occuredAt = block.number;
+                draws[cid].entropyPending = false;
                 draws[cid].completed = true;
                 from += bytesNeeded;
                 removePending(cid);
                 emit DrawCompleted(cid, draws[cid].entropy);
+            } else {
+                revert DrawAlreadyCompleted(cid);
             }
         }
     }
 
 
+    /*** Getters ***/
+
     function getRequestStatus(
         uint256 _requestId
     ) external view returns (bool fulfilled, uint256[] memory randomWords) {
-        require(s_requests[_requestId].exists, "request not found");
+
         RequestStatus memory request = s_requests[_requestId];
+
+        if (!request.exists) {
+            revert RequestDoesNotExist(_requestId);
+        }
+
         return (request.fulfilled, request.randomWords);
+    }
+
+    function getDrawCount() external view onlyOwner returns (uint32) {
+        return drawCount;
+    }
+
+    function getDrawDetails(string memory cid) external view onlyOwner returns (Draw memory) {
+        return draws[cid];
     }
 
     function getRandomnessForDraw(string memory cid) external view returns (bytes memory) {
         return draws[cid].entropy;
     }
+
+    function getPendingDraws() external view onlyOwner returns (string[] memory) {
+        return pendingDraws;
+    }
+
+
+    /*** Setters ***/
+
+    function setSubscription(uint64 subscriptionId) external onlyOwner {
+        s_subscriptionId = subscriptionId;
+    }
+
+
+    /*** Utils ***/
 
     // Division rounds down by default in Solidity, this function rounds up
     function divisionRoundUp(uint32 a, uint32 m) private pure returns (uint32) {
@@ -289,4 +362,5 @@ contract VerifiableDraws is AutomationCompatibleInterface, VRFConsumerBaseV2, Co
             }
         }
     }
+
 }
